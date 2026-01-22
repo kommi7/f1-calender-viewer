e1ea7357d0055df643fc9acb2104b4f995f7a58c
Komal Singh <singhkommi@gmail.com>
2026-01-22 14:29:39 -0500

    Organized and Structured

--- src/main/java/com/f1calendar/controller/RaceController.java
+++ src/main/java/com/f1calendar/controller/RaceController.java
@@ -1,4 +1,4 @@
-package com.f1calender.controller;
+package com.f1calendar.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "f1_calender_vpc" {
+resource "aws_vpc" "f1_calendar_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "f1-calender-vpc"
+    Name = "f1-calendar-vpc"
   }
 }
 
-resource "aws_subnet" "f1_calender_subnet" {
+resource "aws_subnet" "f1_calendar_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.f1_calender_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.f1_calender_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.f1_calendar_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.f1_calendar_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "f1-calender-subnet-${count.index}"
+    Name = "f1-calendar-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "f1_calender_igw" {
-  vpc_id = aws_vpc.f1_calender_vpc.id
+resource "aws_internet_gateway" "f1_calendar_igw" {
+  vpc_id = aws_vpc.f1_calendar_vpc.id
 
   tags = {
-    Name = "f1-calender-igw"
+    Name = "f1-calendar-igw"
   }
 }
 
-resource "aws_route_table" "f1_calender_route_table" {
-  vpc_id = aws_vpc.f1_calender_vpc.id
+resource "aws_route_table" "f1_calendar_route_table" {
+  vpc_id = aws_vpc.f1_calendar_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.f1_calender_igw.id
+    gateway_id = aws_internet_gateway.f1_calendar_igw.id
   }
 
   tags = {
-    Name = "f1-calender-route-table"
+    Name = "f1-calendar-route-table"
   }
 }
 
-resource "aws_route_table_association" "f1_calender_association" {
+resource "aws_route_table_association" "f1_calendar_association" {
   count          = 2
-  subnet_id      = aws_subnet.f1_calender_subnet[count.index].id
-  route_table_id = aws_route_table.f1_calender_route_table.id
+  subnet_id      = aws_subnet.f1_calendar_subnet[count.index].id
+  route_table_id = aws_route_table.f1_calendar_route_table.id
 }
 
-resource "aws_security_group" "f1_calender_cluster_sg" {
-  vpc_id = aws_vpc.f1_calender_vpc.id
+resource "aws_security_group" "f1_calendar_cluster_sg" {
+  vpc_id = aws_vpc.f1_calendar_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "f1_calender_cluster_sg" {
   }
 
   tags = {
-    Name = "f1-calender-cluster-sg"
+    Name = "f1-calendar-cluster-sg"
   }
 }
 
-resource "aws_security_group" "f1_calender_node_sg" {
-  vpc_id = aws_vpc.f1_calender_vpc.id
+resource "aws_security_group" "f1_calendar_node_sg" {
+  vpc_id = aws_vpc.f1_calendar_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,23 +82,23 @@ resource "aws_security_group" "f1_calender_node_sg" {
   }
 
   tags = {
-    Name = "f1-calender-node-sg"
+    Name = "f1-calendar-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "f1_calender" {
-  name     = "f1-calender-cluster"
-  role_arn = aws_iam_role.f1_calender_cluster_role.arn
+resource "aws_eks_cluster" "f1_calendar" {
+  name     = "f1-calendar-cluster"
+  role_arn = aws_iam_role.f1_calendar_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.f1_calender_subnet[*].id
-    security_group_ids = [aws_security_group.f1_calender_cluster_sg.id]
+    subnet_ids         = aws_subnet.f1_calendar_subnet[*].id
+    security_group_ids = [aws_security_group.f1_calendar_cluster_sg.id]
   }
 }
 
 /*
 resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name    = aws_eks_cluster.f1_calender.name
+  cluster_name    = aws_eks_cluster.f1_calendar.name
   addon_name      = "aws-ebs-csi-driver"
   
   resolve_conflicts_on_create = "OVERWRITE"
@@ -106,11 +106,11 @@ resource "aws_eks_addon" "ebs_csi_driver" {
 }
 */
 
-resource "aws_eks_node_group" "f1_calender" {
-  cluster_name    = aws_eks_cluster.f1_calender.name
-  node_group_name = "f1-calender-node-group"
-  node_role_arn   = aws_iam_role.f1_calender_node_group_role.arn
-  subnet_ids      = aws_subnet.f1_calender_subnet[*].id
+resource "aws_eks_node_group" "f1_calendar" {
+  cluster_name    = aws_eks_cluster.f1_calendar.name
+  node_group_name = "f1-calendar-node-group"
+  node_role_arn   = aws_iam_role.f1_calendar_node_group_role.arn
+  subnet_ids      = aws_subnet.f1_calendar_subnet[*].id
 
   scaling_config {
     desired_size = 3
@@ -122,12 +122,12 @@ resource "aws_eks_node_group" "f1_calender" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.f1_calender_node_sg.id]
+    source_security_group_ids = [aws_security_group.f1_calendar_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "f1_calender_cluster_role" {
-  name = "f1-calender-cluster-role"
+resource "aws_iam_role" "f1_calendar_cluster_role" {
+  name = "f1-calendar-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -145,13 +145,13 @@ resource "aws_iam_role" "f1_calender_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "f1_calender_cluster_role_policy" {
-  role       = aws_iam_role.f1_calender_cluster_role.name
+resource "aws_iam_role_policy_attachment" "f1_calendar_cluster_role_policy" {
+  role       = aws_iam_role.f1_calendar_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "f1_calender_node_group_role" {
-  name = "f1-calender-node-group-role"
+resource "aws_iam_role" "f1_calendar_node_group_role" {
+  name = "f1-calendar-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -169,22 +169,22 @@ resource "aws_iam_role" "f1_calender_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "f1_calender_node_group_role_policy" {
-  role       = aws_iam_role.f1_calender_node_group_role.name
+resource "aws_iam_role_policy_attachment" "f1_calendar_node_group_role_policy" {
+  role       = aws_iam_role.f1_calendar_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "f1_calender_node_group_cni_policy" {
-  role       = aws_iam_role.f1_calender_node_group_role.name
+resource "aws_iam_role_policy_attachment" "f1_calendar_node_group_cni_policy" {
+  role       = aws_iam_role.f1_calendar_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "f1_calender_node_group_registry_policy" {
-  role       = aws_iam_role.f1_calender_node_group_role.name
+resource "aws_iam_role_policy_attachment" "f1_calendar_node_group_registry_policy" {
+  role       = aws_iam_role.f1_calendar_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-resource "aws_iam_role_policy_attachment" "f1_calender_node_group_ebs_policy" {
-  role       = aws_iam_role.f1_calender_node_group_role.name
+resource "aws_iam_role_policy_attachment" "f1_calendar_node_group_ebs_policy" {
+  role       = aws_iam_role.f1_calendar_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
